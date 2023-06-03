2cc2fb9fa2a8bf686960167e3e165dd8602ffd45
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-06-03 16:02:09 +0000

    finished everything before the login, not api requests yet

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () =>{
-    return (
+    const navigate = useNavigate()
+    const [user,setUser] = useState({})
+
+    const user_setinator = (event) => {
+        setUser({ ...user, [event.target.id]: event.target.value });
+        console.log(user)
+      };
+
+    const submit_handlinator = (event) =>{
+        event.preventDefault()
+
+        ////agregar aca el post a /signup. hace un alert si devuelve que no esta en la base de datos 
+        console.log(user)
+    }
+
+    return(
         <>
-        
+            <div className="container mt-5">
+                <form onSubmit={e => {submit_handlinator(e)}} >
+                    <div className="mb-3">
+                        <label htmlFor ="email" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={e => {user_setinator(e)}} required/>
+                        
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="password" onChange={e => {user_setinator(e)}} required/>
+                    </div>
+                    
+                    <button type="submit" className="btn btn-primary w-100">Submit</button>
+                </form>
+            </div>
         </>
     )
 }
--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -22,19 +22,19 @@ const Signup = () =>{
     return (
         <>
             <div className="container mt-5">
-                <form onSubmit={e => {submit_handlinator(e)}}>
+                <form onSubmit={e => {submit_handlinator(e)}} >
                     <div className="mb-3">
                         <label htmlFor ="email" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={e => {setUser({ ...user, "email": e.target.value })}}/>
+                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={e => {setUser({ ...user, "email": e.target.value })}} required/>
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password1" onChange={e => {setPassword(e.target.value)}}/>
+                        <input type="password" className="form-control" id="password1" onChange={e => {setPassword(e.target.value)}} required/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password2" className="form-label">Repeat Password</label>
-                        <input type="password" className="form-control" id="password2" onChange={e => {setConfirmPassword(e.target.value)}}/>
+                        <input type="password" className="form-control" id="password2" onChange={e => {setConfirmPassword(e.target.value)}} required/>
                     </div>
                     <button type="submit" className="btn btn-primary w-100">Submit</button>
                 </form>
